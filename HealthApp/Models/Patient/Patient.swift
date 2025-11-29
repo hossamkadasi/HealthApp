f27b730efae9c8131264013b2613569bcfe7d252
hossamkadasi <hossamalkadasi@gmail.com>
2025-11-29 23:51:18 +0300

    Create Patient.swift

---
+++ HealthApp/Models/Patient/Patient.swift
@@ -0,0 +1,15 @@
+import Foundation
+import SwiftData
+
+@Model
+class Patient {
+    // 1. تعريف البيانات
+    var name: String
+    var age: Int
+    
+    // 2. دالة الإنشاء (عشان نقدر نسوي مريض جديد لاحقاً)
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
