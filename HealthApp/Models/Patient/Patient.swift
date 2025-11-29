14a707ce671792043fbaa4015af09db124b2b4dd
hossamkadasi <hossamalkadasi@gmail.com>
2025-11-29 23:56:26 +0300

    Update Patient.swift

--- HealthApp/Models/Patient/Patient.swift
+++ HealthApp/Models/Patient/Patient.swift
@@ -5,7 +5,8 @@ import SwiftData
 class Patient {
     // 1. تعريف البيانات
     var name: String
-    var age: Int
+    var age: @Relationship(deleteRule: .cascade) var operativeNotes: [OperativeNote] = []
+Int
     
     // 2. دالة الإنشاء (عشان نقدر نسوي مريض جديد لاحقاً)
     init(name: String, age: Int) {
